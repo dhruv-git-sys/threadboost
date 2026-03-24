52f4729c4e7ac87da93d47d7ff897b87bf590178
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2026-03-24 16:15:46 +0530

    ThreadPoolConfig

--- src/main/java/com/dhruv/threadboost/config/ThreadPoolConfig.java
+++ src/main/java/com/dhruv/threadboost/config/ThreadPoolConfig.java
@@ -1,4 +1,23 @@
 package com.dhruv.threadboost.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
 public class ThreadPoolConfig {
+    @Bean(name="taskExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor executor=new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("ThreadBoost-");
+        executor.initialize();
+        return executor;
+    }
 }
